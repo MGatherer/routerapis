5474c524a47394ae3df9e835b2992d759a6922f7
Undiscovered <undiscovered@outlook.fr>
2016-05-11 10:59:22 +0200

    Update and rename demo.php to index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-require_once 'vendor/autoload.php';
+date_default_timezone_set('Europe/Paris');
+ini_set('default_charset','utf-8');
+require_once 'CustomHttpClient.php';
+require_once 'Router.php';
 
 //The router class is the main entry point for interaction.
 $router = new HSPDev\HuaweiApi\Router;
 
 //If specified without http or https, assumes http://
-$router->setAddress('192.168.8.1');
+$router->setAddress('192.168.2.1');
 
 //Username and password. 
 //Username is always admin as far as I can tell.
